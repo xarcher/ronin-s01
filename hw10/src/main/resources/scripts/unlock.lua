84280384929667e579947952d4ab6d70f0899606
xarcher <xarcher.dev@gmail.com>
2024-01-16 19:24:27 +0700

    Update unlock.lua

--- hw10/src/main/resources/scripts/unlock.lua
+++ hw10/src/main/resources/scripts/unlock.lua
@@ -2,6 +2,7 @@ local lockKey =  KEYS[1]
 local clientLockRequestId = ARGV[1]
 
 local clientLockIdInRedis = redis.call('GET', lockKey)
+
 if clientLockIdInRedis == clientLockRequestId then
     redis.call('DEL', lockKey)
     return true
